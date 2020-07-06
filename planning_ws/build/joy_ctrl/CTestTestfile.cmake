5010935e6ee69a79b2f1f1bb9432f42537f67e6f
github-bard <amazing_git@163.com>
2020-07-07 07:57:47 +0800

    Delete some files

--- planning_ws/build/joy_ctrl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amazing/submarine/workspace/planning_ws/src/joy_ctrl
-# Build directory: /home/amazing/submarine/workspace/planning_ws/build/joy_ctrl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- planning_ws/build/serial/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/amazing/submarine/workspace/planning_ws/src/serial/include".split(';') if "/home/amazing/submarine/workspace/planning_ws/src/serial/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lserial".split(';') if "-lserial" != "" else []
-PROJECT_NAME = "serial"
-PROJECT_SPACE_DIR = "/home/amazing/submarine/workspace/planning_ws/build/devel"
-PROJECT_VERSION = "1.2.1"
