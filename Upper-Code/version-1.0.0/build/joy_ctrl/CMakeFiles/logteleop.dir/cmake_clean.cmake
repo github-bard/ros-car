a35a042c5cd37808ccb35f77ef65a0ac618b00d8
github-bard <amazing_git@163.com>
2020-07-07 08:20:27 +0800

    Adapting the first version of the Underlying-Code

---
+++ Upper-Code/version-1.0.0/build/joy_ctrl/CMakeFiles/logteleop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logteleop.dir/src/action_tutorials.cpp.o"
+  "../devel/lib/joy_ctrl/logteleop.pdb"
+  "../devel/lib/joy_ctrl/logteleop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logteleop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
